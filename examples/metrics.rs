a4d3df6b420d32333523ea52c4e7f37f9d98db3b
Vincent <andyli386@gmail.com>
2024-12-03 12:32:44 +0800

    refactor: modify Metrics to CmapMetrics

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -1,14 +1,14 @@
 use std::{thread, time::Duration};
 
 use anyhow::Result;
-use concurrency::Metrics;
+use concurrency::CmapMetrics;
 use rand::Rng;
 
 const N: usize = 4;
 const M: usize = 2;
 
 fn main() -> Result<()> {
-    let metrics = Metrics::new();
+    let metrics = CmapMetrics::new();
 
     // println!("{:?}", metrics.snapshot());
     println!("{}", metrics);
@@ -27,7 +27,7 @@ fn main() -> Result<()> {
     }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+fn task_worker(idx: usize, metrics: CmapMetrics) -> Result<()> {
     thread::spawn(move || {
         loop {
             let mut rng = rand::thread_rng();
@@ -40,7 +40,7 @@ fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
     Ok(())
 }
 
-fn request_worker(metrics: Metrics) -> Result<()> {
+fn request_worker(metrics: CmapMetrics) -> Result<()> {
     thread::spawn(move || {
         loop {
             let mut rng = rand::thread_rng();
